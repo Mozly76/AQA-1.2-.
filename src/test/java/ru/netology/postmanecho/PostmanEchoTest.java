5972a0d87713cf5c8b29481b9b94b34a04245c3d
Sergey Kiselev <mozly76@yandex.ru>
2022-09-04 06:01:43 +0300

    Test passed

--- src/test/java/ru/netology/postmanecho/PostmanEchoTest.java
+++ src/test/java/ru/netology/postmanecho/PostmanEchoTest.java
@@ -20,7 +20,7 @@ public class PostmanEchoTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hell"))
+                .body("data", equalTo("Hello"))
         ;
     }
 }
